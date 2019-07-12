4564daaea4f5f060d541f6b08be1b1d3f51ccef0
Konstantin Beloglazov <bk@infoservice.ru>
2019-07-12 16:30:31 +0500

    RUSVINYL-5 Создан компонент для детальных страниц новостей, опросов, анонсов, блогов генерального директора, добавлено следование главного меню и левого при прокурчивании страницы

--- modules/infoservice.rusvinyl/install/templates/rusvinyl/components/bitrix/news.detail/rusvinyl.default/template.php
+++ modules/infoservice.rusvinyl/install/templates/rusvinyl/components/bitrix/news.detail/rusvinyl.default/template.php
@@ -1,95 +1,32 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-/** @var array $arParams */
-/** @var array $arResult */
-/** @global CMain $APPLICATION */
-/** @global CUser $USER */
-/** @global CDatabase $DB */
-/** @var CBitrixComponentTemplate $this */
-/** @var string $templateName */
-/** @var string $templateFile */
-/** @var string $templateFolder */
-/** @var string $componentPath */
-/** @var CBitrixComponent $component */
-$this->setFrameMode(true);
-?>
-<div class="news-detail">
-    <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arResult["DETAIL_PICTURE"])):?>
-        <img
-            class="detail_picture"
-            border="0"
-            src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>"
-            width="<?=$arResult["DETAIL_PICTURE"]["WIDTH"]?>"
-            height="<?=$arResult["DETAIL_PICTURE"]["HEIGHT"]?>"
-            alt="<?=$arResult["DETAIL_PICTURE"]["ALT"]?>"
-            title="<?=$arResult["DETAIL_PICTURE"]["TITLE"]?>"
-            />
-    <?endif?>
-    <?if($arParams["DISPLAY_DATE"]!="N" && $arResult["DISPLAY_ACTIVE_FROM"]):?>
-        <span class="news-date-time"><?=$arResult["DISPLAY_ACTIVE_FROM"]?></span>
-    <?endif;?>
-    <?if($arParams["DISPLAY_NAME"]!="N" && $arResult["NAME"]):?>
-        <h3><?=$arResult["NAME"]?></h3>
-    <?endif;?>
-    <?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arResult["FIELDS"]["PREVIEW_TEXT"]):?>
-        <p><?=$arResult["FIELDS"]["PREVIEW_TEXT"];unset($arResult["FIELDS"]["PREVIEW_TEXT"]);?></p>
-    <?endif;?>
-    <?if($arResult["NAV_RESULT"]):?>
-        <?if($arParams["DISPLAY_TOP_PAGER"]):?><?=$arResult["NAV_STRING"]?><br /><?endif;?>
-        <?echo $arResult["NAV_TEXT"];?>
-        <?if($arParams["DISPLAY_BOTTOM_PAGER"]):?><br /><?=$arResult["NAV_STRING"]?><?endif;?>
-    <?elseif(strlen($arResult["DETAIL_TEXT"])>0):?>
-        <?echo $arResult["DETAIL_TEXT"];?>
-    <?else:?>
-        <?echo $arResult["PREVIEW_TEXT"];?>
-    <?endif?>
-    <div style="clear:both"></div>
-    <br />
-    <?foreach($arResult["FIELDS"] as $code=>$value):
-        if ('PREVIEW_PICTURE' == $code || 'DETAIL_PICTURE' == $code)
-        {
-            ?><?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?
-            if (!empty($value) && is_array($value))
-            {
-                ?><img border="0" src="<?=$value["SRC"]?>" width="<?=$value["WIDTH"]?>" height="<?=$value["HEIGHT"]?>"><?
-            }
-        }
-        else
-        {
-            ?><?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?><?
-        }
-        ?><br />
-    <?endforeach;
-    foreach($arResult["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
+<?
+use Bitrix\Main\{Localization\Loc, Type\DateTime};
 
-        <?=$arProperty["NAME"]?>:&nbsp;
-        <?if(is_array($arProperty["DISPLAY_VALUE"])):?>
-            <?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
-        <?else:?>
-            <?=$arProperty["DISPLAY_VALUE"];?>
-        <?endif?>
-        <br />
-    <?endforeach;
-    if(array_key_exists("USE_SHARE", $arParams) && $arParams["USE_SHARE"] == "Y")
-    {
-        ?>
-        <div class="news-detail-share">
-            <noindex>
-            <?
-            $APPLICATION->IncludeComponent("bitrix:main.share", "", array(
-                    "HANDLERS" => $arParams["SHARE_HANDLERS"],
-                    "PAGE_URL" => $arResult["~DETAIL_PAGE_URL"],
-                    "PAGE_TITLE" => $arResult["~NAME"],
-                    "SHORTEN_URL_LOGIN" => $arParams["SHARE_SHORTEN_URL_LOGIN"],
-                    "SHORTEN_URL_KEY" => $arParams["SHARE_SHORTEN_URL_KEY"],
-                    "HIDE" => $arParams["SHARE_HIDE"],
-                ),
-                $component,
-                array("HIDE_ICONS" => "Y")
-            );
-            ?>
-            </noindex>
-        </div>
-        <?
-    }
-    ?>
+if (!defined("B_PROLOG_INCLUDED") || (B_PROLOG_INCLUDED !== true)) die();?>
+<div class="rusv-news-detail">
+    <div class="rusv-news-list-back">
+        <a
+            class="rusv-news-list-back-link"
+            href="<?=$arResult['LIST_PAGE_URL']?>"><?=
+                Loc::getMessage('RETURN_TO_LIST_LINK_TITLE', ['#NAME#' => $arResult['IBLOCK']['NAME']])
+        ?></a>
+    </div>
+    <div class="rusv-news-detail-title">
+        <span class="rusv-news-detail-title-value"><?=$arResult['NAME']?></span>
+    </div>
+    <div class="rusv-news-detail-date"><?
+        $dateTimeValue = ltrim(FormatDate('d F Y', DateTime::createFromUserTime($arResult['TIMESTAMP_X'])->getTimestamp()), '0');?>
+        <span class="rusv-news-detail-date-value"><?=$dateTimeValue?></span>
+    </div><?
+    $displayImg = is_array($arResult['DETAIL_PICTURE']) ? $arResult['DETAIL_PICTURE']['SRC']
+                : SITE_TEMPLATE_PATH . '/images/news-default.svg';?>
+    <div class="rusv-news-detail-image">
+        <img
+            class="rusv-news-detail-image-object"
+            border="0"
+            src="<?=$displayImg?>">
+    </div>
+    <div class="rusv-news-detail-text">
+        <div class="rusv-news-detail-text-preview"><?=$arResult['PREVIEW_TEXT']?></div>
+        <div class="rusv-news-detail-text-data"><?=$arResult['DETAIL_TEXT']?></div>
+    </div>
 </div>
