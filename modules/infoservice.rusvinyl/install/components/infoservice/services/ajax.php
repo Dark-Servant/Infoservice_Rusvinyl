4bb4be9ab51674c12ee1a10a26baf7c8a5de488f
Konstantin Beloglazov <bk@infoservice.ru>
2019-08-07 12:38:33 +0500

    RUSVINYL-21 Добавлен комментарий в ajax.php

--- modules/infoservice.rusvinyl/install/components/infoservice/services/ajax.php
+++ modules/infoservice.rusvinyl/install/components/infoservice/services/ajax.php
@@ -29,12 +29,20 @@ try {
     if (empty($iblockCode))
         throw new Exception(Loc::getMessage('ERROR_EMPTY_AJAX_IBLOCK_CODE'));
 
+    // У каждого сервиса должен быть свой ajax-обработчик в своем файле
     $ajaxFile = __DIR__ . '/ajax/' . $iblockCode . '.php';
     if (!file_exists($ajaxFile))
         throw new Exception(Loc::getMessage('ERROR_AJAX_FILE_NOT_EXISTS', ['CODE' => $iblockCode]));
     
     require $ajaxFile;
-
+    /**
+        В файле с обработчиком для конкретного сервиса должен лежать класс
+        или он уже должен быть объявлен.        
+        Класс находится в namespace c именем
+        <Название партнера>\<Название модуля>\Ajax\<Название класса сервиса>.
+        <Название партнера>\<Название модуля> будет получено из кода модуля
+        <Название класса сервиса> будет получено из символьного кода инфоблока сервиса
+     */
     $className = implode(
                     '\\', array_map(
                         function($part) {
@@ -49,6 +57,10 @@ try {
                         return strtoupper($part[1]);
                     }, $iblockCode
                 );
+    /**
+        Для каждого обработчика запроса в классе должен быть объявлен
+        обработчик с таким же названием
+     */
     $action = trim($request->get('action'));
     if ($action && method_exists($className, $action)) {
         $answer['data'] = (
